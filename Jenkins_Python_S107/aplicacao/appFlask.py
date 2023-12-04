ec751a82819ce9f05b47f39c74d3008630f36740
LucaFelipe <luca.felipe@ges.inatel.br>
2023-12-04 17:30:09 -0300

    Salvando pequenas mudanças

--- Jenkins_Python_S107/aplicacao/appFlask.py
+++ Jenkins_Python_S107/aplicacao/appFlask.py
@@ -1,32 +1,50 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_caching import Cache
 import redis
+import time
 
 app = Flask(__name__)
 
 # Configuração do MySQL
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://myuser:mypassword@mysql-db/mydatabase'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://myuser:mypassword@mysql-db:3306/mydatabase'
 db = SQLAlchemy(app)
 
 # Configuração do Redis
 app.config['CACHE_TYPE'] = 'redis'
-app.config['CACHE_KEY_PREFIX'] = 'my_cache_prefix'
+app.config['CACHE_KEY_PREFIX'] = 'test_results'
 cache = Cache(app)
+redis_client = redis.StrictRedis(host='redis', port=6379, decode_responses=True)
+
+class TestResult(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    test_name = db.Column(db.String(255), nullable=False)
+    result = db.Column(db.String(255), nullable=False)
+    execution_time = db.Column(db.Float, nullable=False)
 
 @app.route('/')
 def index():
-    # Exemplo de uso do MySQL
-    result = db.session.execute("SELECT 1")
-    mysql_data = result.fetchone()[0]
+    # Exemplo de teste (substitua isso por seus próprios testes)
+    start_time = time.time()
+    test_result = "Resultado do teste"
+    end_time = time.time()
+    execution_time = end_time - start_time
 
-    # Exemplo de uso do Redis
-    cached_data = cache.get('my_cached_data')
-    if not cached_data:
-        cached_data = 'Data não encontrado no cache. Salvando no cache.'
-        cache.set('my_cached_data', cached_data)
+    # Armazena o resultado do teste no MySQL
+    new_result = TestResult(test_name='Teste 1', result=test_result, execution_time=execution_time)
+    db.session.add(new_result)
+    db.session.commit()
 
-    return f'MySQL Data: {mysql_data}, Redis Cached Data: {cached_data}'
+    # Armazena o tempo de execução no Redis
+    cache_key = 'execution_time_test_1'
+    cached_execution_time = cache.get(cache_key)
+    if not cached_execution_time:
+        cache.set(cache_key, execution_time, timeout=None)  # Sem expiração
+
+    # Recupera os resultados dos testes do MySQL
+    test_results = TestResult.query.all()
+
+    return render_template('index.html', test_results=test_results)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
